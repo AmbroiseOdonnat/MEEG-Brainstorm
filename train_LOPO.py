482f48c8fc82154b33ff2ee038b7e316e3d02c98
Théo Gnassounou <p117205@rosenberg.neuro.polymtl.ca>
2022-08-11 11:55:40 -0400

    add wandb for saving data and update name of parameters

--- train_LOPO.py
+++ train_LOPO.py
@@ -7,16 +7,14 @@ Contributors: Ambroise Odonnat and Theo Gnassounou.
 """
 
 import argparse
-import os
+import wandb
 import torch
 
 import numpy as np
-import pandas as pd
 
 from loguru import logger
 from torch import nn
 from torch.optim import Adam
-from torch.optim.lr_scheduler import ReduceLROnPlateau
 
 from models.architectures import EEGNet_1D, RNN_self_attention
 from models.architectures import EEGNet, GTN, STT
@@ -24,7 +22,7 @@ from models.training import make_model
 from loader.dataloader import Loader
 from loader.data import Data
 from utils.losses import get_criterion
-from utils.utils_ import define_device, get_pos_weight
+from utils.utils_ import reset_wandb_env, get_pos_weight
 from utils.utils_ import reset_weights, get_alpha
 from utils.select_subject import select_subject
 from augmentation import AffineScaling, Zoom, ChannelsShuffle
@@ -42,15 +40,15 @@ def get_parser():
                         default="/home/GRAMES.POLYMTL.CA/p117205/"
                                 "duke/projects/ivadomed/MEEG-Brainstorm/"
                                 "EEG_Epilepsy_27_subjects/")
-    parser.add_argument("--method", type=str, default="RNN_self_attention")
-    parser.add_argument("--save", action="store_true")
-    parser.add_argument("--scheduler", action="store_true")
+    parser.add_argument("--architecture",
+                        type=str,
+                        default="RNN_self_attention")
     parser.add_argument("--batch_size", type=int, default=64)
     parser.add_argument("--lr", type=float, default=0.01)
     parser.add_argument("--n_epochs", type=int, default=100)
-    parser.add_argument("--n_good_detection", type=int, default=2)
-    parser.add_argument("--n_subjects", type=int, default=5)
-    parser.add_argument("--selected_subjects", type=str, nargs="+", default=[])
+    parser.add_argument("--patience", type=int, default=5)
+    parser.add_argument("--n_good_detection", type=int, default=1)
+    parser.add_argument("--n_subjects", type=int, default=20)
     parser.add_argument("--weight_loss", action="store_true")
     parser.add_argument("--cost_sensitive", action="store_true")
     parser.add_argument("--lambd", type=float, default=1e-3)
@@ -60,8 +58,6 @@ def get_parser():
     parser.add_argument("--len_trials", type=float, default=2)
     parser.add_argument("--data_augment", type=str, default=None)
     parser.add_argument("--balanced", action="store_true")
-    parser.add_argument("--patience", type=int, default=5)
-    parser.add_argument("--gpu_id", type=int, default=0)
 
     return parser
 
@@ -70,15 +66,13 @@ def get_parser():
 parser = get_parser()
 args = parser.parse_args()
 path_root = args.path_root
-method = args.method
-save = args.save
-scheduler = args.scheduler
+architecture = args.architecture
 batch_size = args.batch_size
 lr = args.lr
 n_epochs = args.n_epochs
+patience = args.patience
 n_good_detection = args.n_good_detection
 n_subjects = args.n_subjects
-selected_subjects = args.selected_subjects
 weight_loss = args.weight_loss
 cost_sensitive = args.cost_sensitive
 lambd = args.lambd
@@ -88,13 +82,11 @@ alpha = args.alpha
 len_trials = args.len_trials
 data_augment = args.data_augment
 balanced = args.balanced
-patience = args.patience
-gpu_id = args.gpu_id
 # Recover params
 weight_decay = 0
 
 # Define device
-available, device = define_device(gpu_id)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 # Define loss
 criterion = nn.BCEWithLogitsLoss().to(device)
@@ -105,10 +97,14 @@ mean_acc, mean_f1, mean_precision, mean_recall = 0, 0, 0, 0
 steps = 0
 
 # Recover dataset
-assert method in ("EEGNet", "EEGNet_1D", "GTN",
-                  "RNN_self_attention", "STT")
-logger.info(f"Method used: {method}")
-if method in ["EEGNet_1D", "RNN_self_attention"]:
+assert architecture in ("EEGNet",
+                        "EEGNet_1D",
+                        "GTN",
+                        "RNN_self_attention",
+                        "STT")
+
+logger.info(f"architecture used: {architecture}")
+if architecture in ["EEGNet_1D", "RNN_self_attention"]:
     single_channel = True
 else:
     single_channel = False
@@ -116,11 +112,10 @@ else:
 path_subject_info = (
     "results/info_subject_{}".format(len_trials)
 )
-if selected_subjects == []:
-    selected_subjects = select_subject(n_subjects,
-                                       path_subject_info,
-                                       path_root,
-                                       len_trials)
+selected_subjects = select_subject(n_subjects,
+                                   path_subject_info,
+                                   path_root,
+                                   len_trials)
 
 dataset = Data(path_root,
                "spikeandwave",
@@ -173,138 +168,83 @@ for gen_seed in range(1):
                  for _ in range(len(selected_subjects))]
     for i, test_subject_id in enumerate(subject_ids):
         seed = seed_list[i]
-        # Labels are the spike events times
-        loader = Loader(data,
-                        labels,
-                        annotated_channels,
-                        single_channel=single_channel,
-                        batch_size=batch_size,
-                        balanced=balanced,
-                        data_augment=data_augment,
-                        transforms=transforms,
-                        subject_LOPO=test_subject_id,
-                        seed=seed,
-                        )
-        train_loader, val_loader, test_loader, train_labels = loader.load()
+        config = vars(args)
+        config.update({"test_subject_id": test_subject_id,
+                       "seed": seed})
+        reset_wandb_env()
+        with wandb.init(reinit=True, config=config):
+            # Labels are the spike events times
+            loader = Loader(data,
+                            labels,
+                            annotated_channels,
+                            single_channel=single_channel,
+                            batch_size=batch_size,
+                            balanced=balanced,
+                            data_augment=data_augment,
+                            transforms=transforms,
+                            subject_LOPO=test_subject_id,
+                            seed=seed,
+                            )
+            train_loader, val_loader, test_loader, train_labels = loader.load()
 
-        # Define architecture
-        if method == "EEGNet":
-            n_time_points = len(data[subject_ids[0]][0][0][0])
-            architecture = EEGNet()
-        if method == "EEGNet_1D":
-            architecture = EEGNet_1D()
-        elif method == "GTN":
-            n_time_points = len(data[subject_ids[0]][0][0][0])
-            architecture = GTN(n_time_points=n_time_points)
-        elif method == "RNN_self_attention":
-            n_time_points = len(data[subject_ids[0]][0][0][0])
-            architecture = RNN_self_attention(n_time_points=n_time_points)
-        elif method == "STT":
-            n_time_points = len(data[subject_ids[0]][0][0][0])
-            architecture = STT(n_time_points=n_time_points)
-        architecture.apply(reset_weights)
+            # Define architecture
+            if architecture == "EEGNet":
+                n_time_points = len(data[subject_ids[0]][0][0][0])
+                architecture = EEGNet()
+            elif architecture == "EEGNet_1D":
+                architecture = EEGNet_1D()
+            elif architecture == "GTN":
+                n_time_points = len(data[subject_ids[0]][0][0][0])
+                architecture = GTN(n_time_points=n_time_points)
+            elif architecture == "RNN_self_attention":
+                n_time_points = len(data[subject_ids[0]][0][0][0])
+                architecture = RNN_self_attention(n_time_points=n_time_points)
+            elif architecture == "STT":
+                n_time_points = len(data[subject_ids[0]][0][0][0])
+                architecture = STT(n_time_points=n_time_points)
+            architecture.apply(reset_weights)
 
-        # Define training loss
-        if weight_loss:
-            pos_weight = get_pos_weight(train_labels).to(device)
-            train_criterion = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
-            train_criterion = train_criterion.to(device)
-        else:
-            train_criterion = criterion
-        if focal:
-            alpha = get_alpha(train_labels)
-        train_criterion = get_criterion(train_criterion,
-                                        cost_sensitive,
-                                        lambd,
-                                        focal,
-                                        alpha,
-                                        gamma)
-        # Define optimizer
-        optimizer = Adam(architecture.parameters(),
-                         lr=lr,
-                         weight_decay=weight_decay)
-        if scheduler:
-            scheduler = ReduceLROnPlateau(optimizer, "min", patience=5)
-        else:
-            scheduler = None
-        # Define training pipeline
-        architecture = architecture.to(device)
-        model = make_model(architecture,
-                           train_loader,
-                           val_loader,
-                           optimizer,
-                           scheduler,
-                           train_criterion,
-                           criterion,
-                           single_channel=single_channel,
-                           n_epochs=n_epochs,
-                           patience=patience,
-                           n_good_detection=n_good_detection)
+            # Define training loss
+            if weight_loss:
+                pos_weight = get_pos_weight(train_labels).to(device)
+                train_criterion = nn.BCEWithLogitsLoss(pos_weight=pos_weight)
+                train_criterion = train_criterion.to(device)
+            else:
+                train_criterion = criterion
+            if focal:
+                alpha = get_alpha(train_labels)
+            train_criterion = get_criterion(train_criterion,
+                                            cost_sensitive,
+                                            lambd,
+                                            focal,
+                                            alpha,
+                                            gamma)
+            # Define optimizer
+            optimizer = Adam(architecture.parameters(),
+                             lr=lr,
+                             weight_decay=weight_decay)
 
-        # Train Model
-        best_model, history = model.train()
+            # Define training pipeline
+            architecture = architecture.to(device)
+            model = make_model(architecture,
+                               train_loader,
+                               val_loader,
+                               optimizer,
+                               train_criterion,
+                               criterion,
+                               single_channel=single_channel,
+                               n_epochs=n_epochs,
+                               patience=patience,
+                               n_good_detection=n_good_detection)
 
-        torch.save(
-            best_model.state_dict(),
-            "results/model/model_{}_{}_{}s_{}"
-            "_subjects".format(method,
-                               test_subject_id,
-                               len_trials,
-                               len(selected_subjects)),
-        )
+            # Train Model
+            best_model = model.train()
 
-        # Compute test performance and save it
-        acc, f1, precision, recall = model.score(test_loader)
-        results.append(
-            {
-                "method": method,
-                "lr": lr,
-                "batch_size": batch_size,
-                "weight_loss": weight_loss,
-                "cost_sensitive": cost_sensitive,
-                "focal": focal,
-                "len_trials": len_trials,
-                "n_good_detection": n_good_detection,
-                "test_subject_id": test_subject_id,
-                "balanced": balanced,
-                "data_augment": data_augment,
-                "fold": seed,
-                "acc": acc,
-                "f1": f1,
-                "precision": precision,
-                "recall": recall
-            }
-        )
-        mean_acc += acc
-        mean_f1 += f1
-        mean_precision += precision
-        mean_recall += recall
-        steps += 1
-
-        if save:
-
-            # Save results file as csv
-            if not os.path.exists("results"):
-                os.mkdir("results")
-
-            results_path = (
-                "results/csv_LOPO_hyparameters"
-            )
-            if not os.path.exists(results_path):
-                os.mkdir(results_path)
-
-            results_path = os.path.join(results_path,
-                                        "results_LOPO_spike_detection_{}"
-                                        "-subjects.csv".format(
-                                                        len(subject_ids))
-                                        )
-            df_results = pd.DataFrame(results)
-            results = []
-
-            if os.path.exists(results_path):
-                df_old_results = pd.read_csv(results_path)
-                df_results = pd.concat([df_old_results, df_results])
-            df_results.to_csv(results_path, index=False)
+            acc, f1, precision, recall = model.score(test_loader)
+            wandb.summary['test_acc'] = acc
+            wandb.summary['test_f1'] = f1
+            wandb.summary['test_precision'] = precision
+            wandb.summary['test_recall'] = recall
 
     print("Mean accuracy \t Mean F1-score \t Mean precision \t Mean recall")
     print("-" * 80)
