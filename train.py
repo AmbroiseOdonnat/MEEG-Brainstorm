9752ea8508d0a706ef1d7be57ca121b9dff1847d
Théo Gnassounou <p117205@rosenberg.neuro.polymtl.ca>
2022-08-11 11:53:02 -0400

    update of train.py with new loading data and options

--- train.py
+++ train.py
@@ -8,6 +8,9 @@ Contributors: Ambroise Odonnat and Theo Gnassounou.
 
 import argparse
 import os
+import torch
+import wandb
+
 import numpy as np
 import pandas as pd
 
@@ -21,9 +24,8 @@ from models.training import make_model
 from loader.dataloader import Loader
 from loader.data import Data
 from utils.losses import get_criterion
-from utils.learning_rate_warmup import NoamOpt
-from utils.utils_ import define_device, get_pos_weight, reset_weights
-from augmentation import AffineScaling, ChannelsShuffle, FrequencyShift
+from utils.utils_ import get_pos_weight, reset_weights, get_alpha
+from augmentation import AffineScaling, ChannelsShuffle, Zoom
 from utils.select_subject import select_subject
 
 
@@ -32,54 +34,65 @@ def get_parser():
     """ Set parameters for the experiment."""
 
     parser = argparse.ArgumentParser(
-        "Spike detection", description="Spike detection using attention layer"
+        "Spike detection", description="Spike detection"
     )
-    parser.add_argument("--path_root", type=str,
-                        default="../BIDSdataset/Epilepsy_dataset/")
-    parser.add_argument("--method", type=str, default="RNN_self_attention")
-    parser.add_argument("--save", action="store_true")
-    parser.add_argument("--warmup", action="store_true")
-    parser.add_argument("--batch_size", type=int, default=16)
-    parser.add_argument("--num_workers", type=int, default=0)
+    parser.add_argument("--path_root",
+                        type=str,
+                        default="/home/GRAMES.POLYMTL.CA/p117205/"
+                                "duke/projects/ivadomed/MEEG-Brainstorm/"
+                                "EEG_Epilepsy_27_subjects/")
+    parser.add_argument("--architecture",
+                        type=str,
+                        default="RNN_self_attention")
+    parser.add_argument("--batch_size", type=int, default=64)
+    parser.add_argument("--lr", type=float, default=0.01)
     parser.add_argument("--n_epochs", type=int, default=100)
-    parser.add_argument("--n_subjects", type=int, default=5)
-    parser.add_argument("--selected_subjects", type=str, nargs="+", default=[])
+    parser.add_argument("--patience", type=int, default=5)
+    parser.add_argument("--n_good_detection", type=int, default=1)
+    parser.add_argument("--n_subjects", type=int, default=20)
     parser.add_argument("--weight_loss", action="store_true")
     parser.add_argument("--cost_sensitive", action="store_true")
-    parser.add_argument("--lambd", type=float, default=1e-4)
+    parser.add_argument("--lambd", type=float, default=1e-3)
+    parser.add_argument("--focal", action="store_true")
+    parser.add_argument("--gamma", type=float, default=2)
+    parser.add_argument("--alpha", type=float, default=0.7)
     parser.add_argument("--len_trials", type=float, default=2)
-    parser.add_argument("--transform", action="store_true")
-    parser.add_argument("--patience", type=int, default=10)
+    parser.add_argument("--data_augment", type=str, default=None)
+    parser.add_argument("--balanced", action="store_true")
 
     return parser
 
 
-# Experiment name
+# Recover paths and method
 parser = get_parser()
 args = parser.parse_args()
 path_root = args.path_root
-method = args.method
-save = args.save
-warmup = args.warmup
+architecture = args.architecture
 batch_size = args.batch_size
-num_workers = args.num_workers
+lr = args.lr
 n_epochs = args.n_epochs
+patience = args.patience
+n_good_detection = args.n_good_detection
 n_subjects = args.n_subjects
-selected_subjects = args.selected_subjects
 weight_loss = args.weight_loss
 cost_sensitive = args.cost_sensitive
 lambd = args.lambd
+focal = args.focal
+gamma = args.gamma
+alpha = args.alpha
 len_trials = args.len_trials
-transform = args.transform
-patience = args.patience
+data_augment = args.data_augment
+balanced = args.balanced
 
 # Recover params
-gpu_id = 0
 weight_decay = 0
 lr = 1e-3  # Learning rate
 
 # Define device
-available, device = define_device(gpu_id)
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Define loss
+criterion = nn.BCEWithLogitsLoss().to(device)
 
 # Recover results
 results = []
@@ -87,18 +100,22 @@ mean_acc, mean_f1, mean_precision, mean_recall = 0, 0, 0, 0
 steps = 0
 
 # Recover dataset
-assert method in ("EEGNet", "EEGNet_1D", "GTN",
-                  "RNN_self_attention", "STT", "STTNet")
-logger.info(f"Method used: {method}")
-if method in ["EEGNet_1D", "RNN_self_attention"]:
+assert architecture in ("EEGNet",
+                        "EEGNet_1D",
+                        "GTN",
+                        "RNN_self_attention",
+                        "STT")
+
+logger.info(f"architecture used: {architecture}")
+if architecture in ["EEGNet_1D", "RNN_self_attention"]:
     single_channel = True
 else:
     single_channel = False
 
 path_subject_info = (
-    "../results/info_subject_{}".format(len_trials)
+    "results/info_subject_{}".format(len_trials)
 )
-if selected_subjects == []:
+
     selected_subjects = select_subject(n_subjects,
                                        path_subject_info,
                                        path_root,
@@ -112,64 +129,67 @@ dataset = Data(path_root,
 data, labels, annotated_channels = dataset.all_datasets()
 subject_ids = np.asarray(list(data.keys()))
 
-
-# Define loss
-criterion = nn.BCEWithLogitsLoss().to(device)
-for seed in range(5):
-
-    # Dataloader
-
     # Define transform for data augmentation
-    if transform:
+if data_augment == "online":
 
         affine_scaling = AffineScaling(
-            probability=.5,  # defines the probability of modifying the input
-            a_min=0.7,
-            a_max=1.3,
+        probability=0.5,  # defines the probability of modifying the input
+        a_min=0.5,
+        a_max=1.7,
         )
 
-        frequency_shift = FrequencyShift(
-            probability=.5,  # defines the probability of modifying the input
-            sfreq=128,
-            max_delta_freq=.2
+    zoom = Zoom(
+        probability=0.5,  # defines the probability of modifying the input
+        coeff=0.1,
         )
 
         channels_shuffle = ChannelsShuffle(
-            probability=.5,  # defines the probability of modifying the input
+        probability=0.5,  # defines the probability of modifying the input
             p_shuffle=0.2
         )
 
-        transforms = [affine_scaling, frequency_shift, channels_shuffle]
+    if single_channel:
+        transforms = [affine_scaling, zoom]
     else:
+        transforms = [affine_scaling, zoom, channels_shuffle]
+
+elif data_augment == "offline":
         transforms = None
+
+else:
+    transforms = None
+    data_augment = False
+
+# Dataloader
+config = vars(args)
 
     loader = Loader(data,
                     labels,
                     annotated_channels,
                     single_channel=single_channel,
                     batch_size=batch_size,
+                balanced=balanced,
+                data_augment=data_augment,
                     transforms=transforms,
-                    subject_LOPO=None,
-                    num_workers=num_workers,
                     )
 
     train_loader, val_loader, test_loader, train_labels = loader.load()
 
     # Define architecture
-    if method == "EEGNet":
+if architecture == "EEGNet":
         architecture = EEGNet()
         warmup = False
-    if method == "EEGNet_1D":
+if architecture == "EEGNet_1D":
         architecture = EEGNet_1D()
         warmup = False
-    elif method == "GTN":
+elif architecture == "GTN":
         n_time_points = len(data[subject_ids[0]][0][0][0])
         architecture = GTN(n_time_points=n_time_points)
-    elif method == "RNN_self_attention":
+elif architecture == "RNN_self_attention":
         n_time_points = len(data[subject_ids[0]][0][0][0])
         architecture = RNN_self_attention(n_time_points=n_time_points)
         warmup = False
-    elif method == "STT":
+elif architecture == "STT":
         n_time_points = len(data[subject_ids[0]][0][0][0])
         architecture = STT(n_time_points=n_time_points)
     architecture.apply(reset_weights)
@@ -180,14 +200,20 @@ for seed in range(5):
         train_criterion = train_criterion.to(device)
     else:
         train_criterion = criterion
+
+if focal:
+    alpha = get_alpha(train_labels)
     train_criterion = get_criterion(train_criterion,
                                     cost_sensitive,
-                                    lambd)
+                                lambd,
+                                focal,
+                                alpha,
+                                gamma)
 
     # Define optimizer
-    optimizer = Adam(architecture.parameters(), lr=lr,
+optimizer = Adam(architecture.parameters(),
+                 lr=lr,
                      weight_decay=weight_decay)
-    warm_optimizer = NoamOpt(optimizer)
 
     # Define training pipeline
     architecture = architecture.to(device)
@@ -196,21 +222,19 @@ for seed in range(5):
                        train_loader,
                        val_loader,
                        optimizer,
-                       warmup,
-                       warm_optimizer,
                        train_criterion,
                        criterion,
-                       single_channel,
+                   single_channel=single_channel,
                        n_epochs=n_epochs,
-                       patience=patience)
+                   patience=patience,
+                   n_good_detection=n_good_detection)
 
     # Train Model
-    history = model.train()
+best_model = model.train()
 
     if not os.path.exists("../results"):
         os.mkdir("../results")
 
-    # Compute test performance and save it
     acc, f1, precision, recall = model.score(test_loader)
     results.append(
         {
